edc2494c0c82ff87bb32f9a1328c02a1f533b1a0
Matthias Hamacher <hamachermatthias12@gmail.com>
2020-12-29 18:45:52 +0100

    removed utils file

--- src/INTERACTION/converter.py
+++ src/INTERACTION/converter.py
@@ -14,7 +14,117 @@ from commonroad.prediction.prediction import TrajectoryPrediction
 from commonroad.common.file_reader import CommonRoadFileReader
 from commonroad.common.file_writer import CommonRoadFileWriter, OverwriteExistingFile
 
-from src.INTERACTION.utils import generate_planning_problems
+def generate_planning_problems(scenario, num_problems_desired, id_segment, index, time_end_scenario,
+                               time_start_scenario=0, distance_travel_min=30.0, flag_add_goal_position=False,
+                               orientation_half_range=0.2, velocity_half_range=5, time_step_half_range=10,
+                               position_length=2.0, position_width=2.0, flag_same_direction_problems=False,
+                               thresh_distance_coop=25.0, thresh_orient_coop=1.0, flag_verbose=False,
+                               num_planning_problems: int = 1, keep_ego: bool = False):
+    # final list to hold planning problems
+    list_planning_problems = []
+
+    if not len(scenario.dynamic_obstacles):
+        if flag_verbose: print(
+            f"\t\tScenario with id {scenario.benchmark_id} has no dynamic obstacle, creation of planning problem skipped.")
+        return None, list_planning_problems
+
+    obstacles_ordered = []
+    for obs in scenario.dynamic_obstacles:
+        # we require that the vehicles has moved at least for a certain distance
+        if np.linalg.norm(
+                obs.initial_state.position - obs.prediction.trajectory.final_state.position) < distance_travel_min:
+            continue
+
+        obstacles_ordered.append(obs)
+
+    if not obstacles_ordered:
+        return None, list_planning_problems
+
+    # make a reverse list of obstacles base on their duration
+    obstacles_ordered.sort(
+        key=lambda obs: (obs.prediction.trajectory.final_state.time_step - obs.initial_state.time_step))
+    obstacles_ordered.reverse()
+
+    # some candidate indices to retrieve obstacles to form diverse scenarios
+    list_idx_retrieve = [(id_segment + index * 2) % 20,
+                         (id_segment + index) % 20,
+                         (index * 2) % 20,
+                         index % 20,
+                         0]
+
+    obstacles_candidate = []
+    if num_problems_desired == 1:
+        # individual planning problem
+        # try retrieving obstacles using the list of indices
+        for idx in list_idx_retrieve:
+            try:
+                obstacles_candidate.append(obstacles_ordered[idx])
+            except IndexError:
+                continue
+            else:
+                break
+    else:
+        # cooperative planning
+        # try retrieving obstacles using the list of indices
+        for idx in list_idx_retrieve:
+            try:
+                obstacles_candidate.append(obstacles_ordered[idx])
+            except IndexError:
+                continue
+            else:
+                constraint = obstacles_candidate[0].initial_state
+                id_constraint = obstacles_candidate[0].obstacle_id
+                # try adding companions for this poor guy
+                for obs in obstacles_ordered:
+                    # ensure all proposed problems for cooperative scenarios start near to each other. if required, going in the same direction as well
+                    # skip if (1) the distance of current obstacle to constraint is over a threshold,
+                    # or if (2) they have different driving direction (if examined)
+                    if np.linalg.norm(constraint.position - obs.initial_state.position) > thresh_distance_coop or \
+                            (flag_same_direction_problems and abs(
+                                constraint.orientation - obs.initial_state.orientation) > thresh_orient_coop) or \
+                            obs.obstacle_id == id_constraint:
+                        continue
+                    else:
+                        obstacles_candidate.append(obs)
+
+                break
+
+    # now we choose final planning problems from the candidate ones
+    obstacles_chosen = []
+
+    if num_problems_desired == 1:
+        obstacles_chosen.append(obstacles_candidate[0])
+    else:
+        # if less than 2 candidate obstacles are present
+        if len(obstacles_candidate) < 2:
+            return None, list_planning_problems
+
+        # distribute generated problems evenly
+        index_offset = (len(obstacles_candidate) - 1) // (num_problems_desired - 1)
+        if not index_offset:
+            index_offset = 1
+
+        for i in range(0, len(obstacles_candidate), index_offset):
+            obstacles_chosen.append(obstacles_candidate[i])
+
+    # turn these obstacles into planning problems
+    for i, obs in enumerate(obstacles_chosen):
+        if i >= num_planning_problems:
+            break
+        # TODO maybe num_planning_problems cut off when obstacles get too much
+        from src.planning_problem_utils import generate_planning_problem
+
+        if len(scenario.dynamic_obstacles) > (1 if not keep_ego else 0):
+            list_planning_problems.append(generate_planning_problem(scenario, orientation_half_range, velocity_half_range,
+                                                                    time_step_half_range, keep_ego, obs))
+
+    if len(list_planning_problems) < num_problems_desired:
+        if flag_verbose: print(
+            f"\t\tDesired number of planning problems for scenario <{scenario.benchmark_id}>: {num_problems_desired}, "
+            f"created: {len(list_planning_problems)}")
+
+    return scenario, list_planning_problems
+
 
 
 def get_velocity(track_df: pd.DataFrame) -> np.array:
