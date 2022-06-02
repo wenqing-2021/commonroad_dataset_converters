b235ce679a9a37ed63858ea558e64cacac39a2e9
spx <spx1253029453@163.com>
2022-06-02 17:29:45 +0200

    add round commond

--- commonroad_dataset_converter/planning_problem_utils.py
+++ commonroad_dataset_converter/planning_problem_utils.py
@@ -1,22 +1,5 @@
-__author__ = "Niels Mündler, Xiao Wang, Michael Feil"
-__copyright__ = "TUM Cyber-Physical System Group"
-__credits__ = [""]
-__version__ = "1.0"
-__maintainer__ = "Xiao Wang"
-__email__ = "xiao.wang@tum.de"
-__status__ = "Released"
-
-__desc__ = """
-    Utilities for creating planning problems in the Converters
-"""
-
 import random
-from enum import Enum
-from typing import Type
-import warnings
-import sys
 
-from commonroad.scenario.lanelet import LaneletNetwork
 from commonroad.scenario.trajectory import State
 from commonroad.common.util import Interval, AngleInterval
 from commonroad.geometry.shape import Rectangle
@@ -25,30 +8,14 @@ from commonroad.planning.goal import GoalRegion
 from commonroad.scenario.scenario import Scenario
 from commonroad.scenario.obstacle import ObstacleType, DynamicObstacle
 
-try:
-    from commonroad_route_planner.route_planner import RoutePlanner
-except ModuleNotFoundError as exp:
-    RoutePlanner = None
-    warnings.warn("module commonroad_route_planner not installed, routability check will be skipped.")
 
 class NoCarException(Exception):
     pass
 
 
-class Routability(Enum):
-    ANY = 0
-    # REGULAR_ANDREVERSED = 1
-    REGULAR_STRICT = 2
-
-    @classmethod
-    def options(cls):
-        return [type(item) for item in cls]
-
-
 def obstacle_to_planning_problem(obstacle: DynamicObstacle, planning_problem_id: int, final_time_step=None,
                                  orientation_half_range: float = 0.2,
-                                 velocity_half_range: float = 10, time_step_half_range: int = 25,
-                                 lanelet_network: LaneletNetwork = None, highD: bool = False):
+                                 velocity_half_range: float = 10, time_step_half_range: int = 25):
     """
     Generates planning problem using initial and final states of a DynamicObstacle
     """
@@ -66,31 +33,9 @@ def obstacle_to_planning_problem(obstacle: DynamicObstacle, planning_problem_id:
 
     time_step_interval = Interval(0, final_time_step)
 
-    goal_shape = Rectangle(length=dynamic_obstacle_shape.length + 2.0,
-                           width=max(dynamic_obstacle_shape.width + 1.0, 3.5),
-                           center=dynamic_obstacle_final_state.position,
-                           orientation=dynamic_obstacle_final_state.orientation)
-    # find goal lanelet # TODO: goal lanelet is too large for highD
-    goal_lanelets = lanelet_network.find_lanelet_by_position([dynamic_obstacle_final_state.position])
-    if len(goal_lanelets[0]) == 0:
-        raise NoCarException("Selected final state for planning problem is out of road. Skipping this scenario")
-
-    goal_lanelet_id = goal_lanelets[0][0]
-    goal_lanelet = lanelet_network.find_lanelet_by_id(goal_lanelet_id)
-    if not highD:
-        goal_lanelet_polygon = goal_lanelet.convert_to_polygon()
-        if goal_lanelet_polygon.shapely_object.area > goal_shape.shapely_object.area:
-            goal_position = goal_lanelet_polygon
-        else:
-            goal_position = goal_shape
-    else:
-        # works only for highD because of constant lane width
-        lanelet_width = abs(goal_lanelet.left_vertices[-1][1] - goal_lanelet.right_vertices[-1][1])
-        goal_position = Rectangle(length=20,
-                                  width=lanelet_width,
-                                  center=dynamic_obstacle_final_state.position,
-                                  orientation=0.)
-
+    goal_position = Rectangle(dynamic_obstacle_shape.length, dynamic_obstacle_shape.width,
+                              center=dynamic_obstacle_final_state.position,
+                              orientation=dynamic_obstacle_final_state.orientation)
     goal_region = GoalRegion([State(position=goal_position, orientation=orientation_interval,
                                     velocity=velocity_interval, time_step=time_step_interval)])
 
@@ -100,9 +45,9 @@ def obstacle_to_planning_problem(obstacle: DynamicObstacle, planning_problem_id:
     return PlanningProblem(planning_problem_id, dynamic_obstacle_initial_state, goal_region)
 
 
-def generate_planning_problem(scenario: Scenario, orientation_half_range: float = 0.2, velocity_half_range: float = 10.,
+def generate_planning_problem(scenario: Scenario, orientation_half_range: float = 0.2, velocity_half_range: float = 10,
                               time_step_half_range: int = 25, keep_ego: bool = False,
-                              highD: bool = False, lane_change: bool = False) -> PlanningProblem:
+                              dynamic_obstacle_selected=None) -> PlanningProblem:
     """
     Generates planning problem for scenario by taking obstacle trajectory
     :param scenario: CommonRoad scenario
@@ -110,41 +55,15 @@ def generate_planning_problem(scenario: Scenario, orientation_half_range: float 
     :param velocity_half_range: parameter for goal state velocity
     :param time_step_half_range: parameter for goal state time step
     :param keep_ego: boolean indicating if vehicles selected for planning problem should be kept in scenario
-    :param highD: indicator for highD dataset; select only vehicles that drive to the end of the road
+    :param dynamic_obstacle_selected: currently just for testing the interaction dataset
     :return: CommonRoad planning problem
     """
     # random choose obstacle as ego vehicle
     random.seed(0)
 
-    if len(scenario.dynamic_obstacles) == 0:
-        raise NoCarException("There is no car in dynamic obstacles which can be used as planning problem.")
-
-    max_time_step = max([obstacle.prediction.final_time_step for obstacle in scenario.dynamic_obstacles])
     # only choose car type as ego vehicle
-    car_obstacles = [obstacle for obstacle in scenario.dynamic_obstacles if (obstacle.obstacle_type == ObstacleType.CAR
-                                                                             and obstacle.initial_state.time_step == 0
-                                                                             )]
-    # select only vehicles that drive to the end of the road
-    if highD:
-        car_obstacles_highD = [obs for obs in car_obstacles
-                               if abs(obs.initial_state.position[0] -
-                                      obs.state_at_time(obs.prediction.final_time_step).position[0]) > 100.]
-        if lane_change:
-            car_lane_changing = []
-            lanelet_network = scenario.lanelet_network
-            for obs in car_obstacles_highD:
-                initial_lanelet = lanelet_network.find_lanelet_by_position([obs.initial_state.position])[0][0]
-                final_lanelet = lanelet_network.find_lanelet_by_position([
-                    obs.state_at_time(obs.prediction.final_time_step).position])[0][0]
-                if initial_lanelet != final_lanelet:
-                    car_lane_changing.append(obs)
-            if len(car_lane_changing) == 0:
-                warnings.warn("No lane changing vehicle available, using lane keeping vehicle as planning problem")
-            else:
-                car_obstacles_highD = car_lane_changing
-
-        car_obstacles = car_obstacles_highD
-
+    car_obstacles = [obstacle for obstacle in scenario.dynamic_obstacles if obstacle.obstacle_type == ObstacleType.CAR
+                     and obstacle.initial_state.time_step == 0]
     if len(car_obstacles) > 0:
         dynamic_obstacle_selected = random.choice(car_obstacles)
     else:
@@ -168,39 +87,6 @@ def generate_planning_problem(scenario: Scenario, orientation_half_range: float 
                                                     final_time_step=final_time_step,
                                                     orientation_half_range=orientation_half_range,
                                                     velocity_half_range=velocity_half_range,
-                                                    time_step_half_range=time_step_half_range,
-                                                    lanelet_network=scenario.lanelet_network,
-                                                    highD=highD)
+                                                    time_step_half_range=time_step_half_range)
 
     return planning_problem
-
-
-def check_routability_planning_problem(
-    scenario: Scenario, planning_problem: PlanningProblem,
-    max_difficulity: Type[Routability]
-) -> bool:
-    """
-    Checks if a planning problem is routable on scenario
-    :param scenario: CommonRoad scenario
-    :param planning_problem: Planning Problem to be solved
-    :param max_difficulity: difficulty until which planing problem is considered routable.
-        Routability.ANY: dont do any checks, always return True
-        Routability.REGULAR_STRICT: only return True if default route planner can find a route
-
-    :return: bool, True if CommonRoad planning problem is routeable with max_difficulity
-    """
-    if max_difficulity == Routability.ANY or RoutePlanner is None:
-        return True
-
-    elif max_difficulity ==  Routability.REGULAR_STRICT:
-        route_planner = RoutePlanner(scenario, planning_problem, backend=RoutePlanner.Backend.NETWORKX_REVERSED)
-        candidate_holder = route_planner.plan_routes()
-        _, num_candiadates = candidate_holder.retrieve_all_routes()
-
-        if num_candiadates > 0:
-            return True  # there are some routes.
-        else:
-            return False
-
-    else:
-        warnings.warn(f"option not defined: {max_difficulity}")
