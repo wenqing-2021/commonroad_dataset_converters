98eac0bc4946d0b519e6f6fa7372f5db9d7bc8d0
Luis Gressenbuch <luis.gressenbuch@tum.de>
2022-11-07 17:52:08 +0100

    Converter for SinD

--- commonroad_dataset_converter/mona/mona_utils.py
+++ commonroad_dataset_converter/mona/mona_utils.py
@@ -1,29 +1,21 @@
 import enum
 import importlib.resources
-import math
 import re
+import warnings
 from dataclasses import dataclass
-from multiprocessing import Pool
 from pathlib import Path
-from typing import Optional, Dict, Any
+from typing import Optional
 
 import numpy as np
 import pandas as pd
 import pyproj
 import scipy.spatial
 from commonroad.common.file_reader import CommonRoadFileReader
-from commonroad.common.file_writer import CommonRoadFileWriter, OverwriteExistingFile
-from commonroad.geometry.shape import Rectangle
-from commonroad.planning.planning_problem import PlanningProblemSet
-from commonroad.prediction.prediction import TrajectoryPrediction
-from commonroad.scenario.obstacle import ObstacleType, DynamicObstacle
-from commonroad.scenario.scenario import Scenario, ScenarioID, Tag
-from commonroad.scenario.trajectory import State, Trajectory
+from commonroad.scenario.scenario import Scenario, ScenarioID
 from pyproj import Transformer
-from tqdm import tqdm
 
-from commonroad_dataset_converter.helper import load_yaml
-from commonroad_dataset_converter.planning_problem_utils import (obstacle_to_planning_problem, )
+from commonroad_dataset_converter.helper import load_yaml, IJobGenerator, JobProcessor, T, TrackIdJobGenerator, \
+    DisjunctiveJobGenerator
 
 
 class MONALocation(enum.Enum):
@@ -38,32 +30,6 @@ class MONADay(enum.Enum):
     d20210804 = "20210804"
     d20210805 = "20210805"
     d20210806 = "20210806"
-
-
-def create_obstacle(track_df: pd.DataFrame, dynamic_obstacle_id):
-    length = track_df.length.values[0]
-    width = track_df.width.values[0]
-
-    dynamic_obstacle_type = ObstacleType(track_df.agent_type.iloc[0])
-    dynamic_obstacle_shape = Rectangle(width=width, length=length)
-
-    time_steps = np.array(track_df.time_step)
-    xs = np.array(track_df.x)
-    ys = np.array(track_df.y)
-    velocities = np.array(track_df.v)
-    orientations = np.array(track_df.psi_rad)
-
-    state_list = []
-    for t, x, y, v, theta in zip(time_steps, xs, ys, velocities, orientations):
-        state_list.append(State(position=np.array([x, y]), velocity=v, orientation=theta, time_step=int(t), ))
-
-    dynamic_obstacle_initial_state = state_list[0]
-
-    dynamic_obstacle_trajectory = Trajectory(state_list[1].time_step, state_list[1:])
-    dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape)
-
-    return DynamicObstacle(dynamic_obstacle_id, dynamic_obstacle_type, dynamic_obstacle_shape,
-                           dynamic_obstacle_initial_state, dynamic_obstacle_prediction, )
 
 
 def transform_coordinates(track_origin, df, map_scenario):
@@ -95,7 +61,7 @@ def get_transform_to_scenario(map_scenario: Scenario, source_origin: np.ndarray,
     return trans
 
 
-def _get_map(config, location):
+def _get_mona_map(config, location):
     with importlib.resources.path("commonroad_dataset_converter.mona.maps", config["maps"][location]) as map_path:
         scenario, _ = CommonRoadFileReader(str(map_path)).open()
     return scenario
@@ -116,126 +82,82 @@ def _get_mona_config():
     return config
 
 
-class MONATrackIdJobGenerator:
-    def __init__(self, traj_df, num_scenarios, track_id):
-        if track_id is not None:
-            track_ids = [track_id]
-        elif num_scenarios is not None:
-            track_ids = np.random.choice(traj_df.track_id.unique(), num_scenarios, replace=False)
-        else:
-            track_ids = traj_df.track_id.unique()
-
-        self.track_ids = track_ids
-        self.traj_df = traj_df
-
-    def __iter__(self):
-        for tid in self.track_ids:
-            track = self.traj_df[self.traj_df.track_id == tid]
-            start_frame = track.frame_id.min()
-            end_frame = track.frame_id.max() + 1
-            yield MONAJob(track_id=tid, df=self.traj_df[
-                (self.traj_df.frame_id >= start_frame) & (self.traj_df.frame_id < end_frame)], )
-
-    def __len__(self):
-        return len(self.track_ids)
+@dataclass
+class MONAJob:
+    df: pd.DataFrame
+    map_suffix: str
+    track_id: Optional[int] = None
 
 
-class MONADisjunctiveJobGenerator:
-    def __init__(self, traj_df, num_scenarios, max_duration):
-        num_time_steps = traj_df.frame_id.max() - traj_df.frame_id.min() + 1
-        if num_scenarios is None:
-            # Either num_scenarios or max_duration must be specified!
-            max_duration = max_duration or 100
-            num_scenarios = math.ceil(num_time_steps / max_duration)
-        elif max_duration is None:
-            max_duration = math.floor(num_time_steps / num_scenarios)
-        self.max_duration = max_duration
+file_name_pattern = re.compile(
+        r"(?P<location>east|west|merge)_(?P<day>2021080[2-6])_(?P<segment>[0-9]{3})_trajectories")
+
+
+class MONATrackIdJobGenerator(IJobGenerator):
+
+    def __init__(self, trajectory_file: Path, location: MONALocation, map_scenario: Scenario, config, num_scenarios: int, track_id: int):
+        self.trajectory_file = trajectory_file
+        self.map_scenario = map_scenario
         self.num_scenarios = num_scenarios
-        self.traj_df = traj_df
+        self.track_id = track_id
+        self.origin = config["origin"]
+        match = file_name_pattern.match(trajectory_file.stem)
+        if match is not None:
+            if match["location"] != location.value:
+                warnings.warn(f"Using different map than trajectory location! {match['location']} != {location.value}")
+            map_suffix = f'{match["day"]}{match["segment"]}'
+        else:
+            map_suffix = ""
+        self.map_suffix = map_suffix
 
-    def __iter__(self):
-        for i in range(self.num_scenarios):
-            yield MONAJob(self.traj_df[(self.traj_df.frame_id >= i * self.max_duration) & (
-                    self.traj_df.frame_id < (i + 1) * self.max_duration)])
+    def __iter__(self) -> T:
+        traj_df = _read_trajectories(self.trajectory_file)
+        transform_coordinates(self.origin, traj_df, self.map_scenario)
+        generator = TrackIdJobGenerator(traj_df, self.num_scenarios, self.track_id)
+        for tid, df in generator:
+            yield MONAJob(df, self.map_suffix, tid)
+
+
+class MONADisjunctiveJobGenerator(IJobGenerator):
+    def __init__(self, trajectory_file: Path, location: MONALocation, map_scenario: Scenario, config, num_scenarios: int, max_duration: int):
+        self.trajectory_file = trajectory_file
+        self.map_scenario = map_scenario
+        self.num_scenarios = num_scenarios
+        self.origin = config["origin"]
+        match = file_name_pattern.match(trajectory_file.stem)
+        if match is not None:
+            if match["location"] != location.value:
+                warnings.warn(f"Using different map than trajectory location! {match['location']} != {location.value}")
+            map_suffix = f'{match["day"]}{match["segment"]}'
+        else:
+            map_suffix = ""
+        self.map_suffix = map_suffix
+        self.max_duration = max_duration
+
+    def __iter__(self) -> MONAJob:
+        traj_df = _read_trajectories(self.trajectory_file)
+        transform_coordinates(self.origin, traj_df, self.map_scenario)
+        generator = DisjunctiveJobGenerator(traj_df, self.num_scenarios, self.max_duration)
+        for df in generator:
+            yield MONAJob(df, self.map_suffix)
 
     def __len__(self):
         return self.num_scenarios
 
 
 @dataclass
-class MONAJob:
-    df: pd.DataFrame
-    track_id: Optional[int] = None
+class MONAProcessor(JobProcessor):
 
-
-@dataclass
-class MONAProcessor:
-    map_scenario: Scenario
-    config: Dict[str, Any]
-    output_dir: Path
-    keep_ego: bool
-    map_suffix: str
+    def _get_scenario_id(self, job: MONAJob) -> ScenarioID:
+        scenario_id = super()._get_scenario_id(job)
+        if self.disjunctive:
+            scenario_id.map_name = scenario_id.map_name[:-1] + job.map_suffix
+        else:
+            scenario_id.map_name += job.map_suffix
+        return scenario_id
 
     def __call__(self, job: MONAJob) -> None:
-        ego_id = job.track_id
-        traj_df = job.df
-        time_steps = (traj_df.frame_id.drop_duplicates().sort_values().to_frame().reset_index(drop=True).reset_index(
+        time_steps = (job.df.frame_id.drop_duplicates().sort_values().to_frame().reset_index(drop=True).reset_index(
                 drop=False).rename(columns={"index": "time_step"}))
-        traj_df = traj_df.merge(time_steps, on="frame_id")
-        dt = np.around(np.mean(np.diff(np.sort(traj_df.unix_timestamp.unique()))), 2)
-        map_name = self.map_scenario.scenario_id.map_name + self.map_suffix
-        if ego_id is None:
-            traj_lens = traj_df.groupby("track_id").time_step.count()
-            ego_id = traj_lens.index[traj_lens.argmax()]
-            # Add a suffix D indicating disjunctive scenarios
-            map_name += "D"
-        start_frame = traj_df.frame_id.min()
-        scenario = Scenario(dt=float(dt), scenario_id=ScenarioID(country_id=self.map_scenario.scenario_id.country_id,
-                                                                 map_name=map_name,
-                                                                 map_id=self.map_scenario.scenario_id.map_id,
-                                                                 obstacle_behavior="T",
-                                                                 configuration_id=start_frame + 1,
-                                                                 prediction_id=ego_id, ),
-                            location=self.map_scenario.location, )
-        scenario.add_objects(self.map_scenario.lanelet_network)
-
-        # Create obstacles
-        for track_id in traj_df.track_id.unique():
-            track_df = traj_df[traj_df.track_id == track_id]
-            if len(track_df) < 2:
-                continue
-            obs = create_obstacle(track_df, int(track_id) + 1000)
-            scenario.add_objects(obs)
-
-        ego_obstacle = scenario.obstacle_by_id(int(ego_id) + 1000)
-        if not self.keep_ego:
-            scenario.remove_obstacle(ego_obstacle)
-
-        planning_problem = obstacle_to_planning_problem(ego_obstacle, planning_problem_id=ego_id,
-                                                        lanelet_network=scenario.lanelet_network, )
-
-        if len(scenario.dynamic_obstacles) == 0:
-            return
-
-        # Write scenario file
-        cw = CommonRoadFileWriter(scenario, PlanningProblemSet([planning_problem]), author=self.config["author"],
-                                  affiliation=self.config["affiliation"], source=self.config["source"],
-                                  tags={Tag.URBAN}, )
-        cw.write_to_file(str(self.output_dir / f"{scenario.scenario_id}.xml"), OverwriteExistingFile.ALWAYS, )
-
-
-def run_processor(generator, processor, num_processes):
-    try:
-        num_scenarios = len(generator)
-        num_processes = min(num_processes, num_scenarios)
-    except TypeError:
-        num_scenarios = None
-
-    if num_processes > 1:
-        with Pool(num_processes) as pool:
-            iterator = pool.imap_unordered(processor, generator)
-            for _ in tqdm(iterator, total=num_scenarios, desc="Creating scenarios", unit="scenario", ):
-                pass
-    else:
-        for params in tqdm(generator, total=num_scenarios, desc="Creating scenarios", unit="scenario"):
-            processor(params)
+        job.df = job.df.merge(time_steps, on="frame_id")
+        super().__call__(job)
